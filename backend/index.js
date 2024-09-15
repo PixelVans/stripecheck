7781d8f0487b16a62a1fec953ffad3514127af60
PixelVans <evansmemba001@gmail.com>
2024-09-15 13:15:37 +0300

    host

--- backend/index.js
+++ backend/index.js
@@ -83,7 +83,7 @@ app.post('/create-checkout-session', async (req, res) => {
 
 
 // Start server on port 5000
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
